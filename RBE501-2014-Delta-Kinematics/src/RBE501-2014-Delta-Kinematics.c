4ab231ba3c7ab432a672bc6267bd60f6539112cf
kharrington <kharrington@neuronrobotics.com>
2014-02-08 16:04:44 -0500

    Adding the kinematics project

---
+++ RBE501-2014-Delta-Kinematics/src/RBE501-2014-Delta-Kinematics.c
@@ -0,0 +1,19 @@
+/*
+ ============================================================================
+ Name        : RBE501-2014-Delta-Kinematics.c
+ Author      : Kevin Harrington
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+
+
+	return 0;
+}
+
