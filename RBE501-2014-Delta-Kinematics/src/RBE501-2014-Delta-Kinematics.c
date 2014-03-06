e3221efa6e5562a76e971ac4d683b098653edc8b
Jared Heiser <Heiser_Jared@hotmail.com>
2014-03-06 18:24:03 -0500

    Added my name to the template. Jared Heiser #4

--- RBE501-2014-Delta-Kinematics/src/RBE501-2014-Delta-Kinematics.c
+++ RBE501-2014-Delta-Kinematics/src/RBE501-2014-Delta-Kinematics.c
@@ -1,7 +1,7 @@
 /*
  ============================================================================
  Name        : RBE501-2014-Delta-Kinematics.c
- Author      : Kevin Harrington, Michael DiBlasi, Bill Calabro and Tigger too
+ Author      : Kevin Harrington, Michael DiBlasi, Bill Calabro, Jared Heiser and Tigger too
  Version     :
  Copyright   : BSD with attribution
  Description : The kinematics system for a Linear slide Delta
