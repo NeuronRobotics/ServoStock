448cb3d2a2cdbf91bcb1a2f01be44d5dbbb0c799
madhephaestus <mad.hephaestus@gmail.com>
2014-08-29 12:58:34 -0400

    removing unused files

--- firmware/nbproject/Makefile-default.mk
+++ firmware/nbproject/Makefile-default.mk
@@ -45,17 +45,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=src/driver/AS5055.c src/driver/ForgeHeater.c src/driver/SPIClient.c src/driver/FlashStorageLocal.c src/driver/Servo.c src/kinematics/KosselKinematics.c src/kinematics/delta.c src/kinematics/FrogLegs.c src/kinematics/CartesianController.c src/motion/Calibration.c src/motion/StepperSim.c src/motion/PID.c src/namespace/Callbacks.c src/namespace/Namespace_bcs_cartesian.c src/namespace/UpstreamPackets.c src/main.c
+SOURCEFILES_QUOTED_IF_SPACED=src/driver/AS5055.c src/driver/ForgeHeater.c src/driver/SPIClient.c src/driver/FlashStorageLocal.c src/driver/Servo.c src/kinematics/KosselKinematics.c src/kinematics/delta.c src/kinematics/FrogLegs.c src/kinematics/CartesianController.c src/motion/Calibration.c src/motion/StepperSim.c src/motion/PID.c src/namespace/Namespace_bcs_cartesian.c src/namespace/UpstreamPackets.c src/main.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/src/driver/AS5055.o ${OBJECTDIR}/src/driver/ForgeHeater.o ${OBJECTDIR}/src/driver/SPIClient.o ${OBJECTDIR}/src/driver/FlashStorageLocal.o ${OBJECTDIR}/src/driver/Servo.o ${OBJECTDIR}/src/kinematics/KosselKinematics.o ${OBJECTDIR}/src/kinematics/delta.o ${OBJECTDIR}/src/kinematics/FrogLegs.o ${OBJECTDIR}/src/kinematics/CartesianController.o ${OBJECTDIR}/src/motion/Calibration.o ${OBJECTDIR}/src/motion/StepperSim.o ${OBJECTDIR}/src/motion/PID.o ${OBJECTDIR}/src/namespace/Callbacks.o ${OBJECTDIR}/src/namespace/Namespace_bcs_cartesian.o ${OBJECTDIR}/src/namespace/UpstreamPackets.o ${OBJECTDIR}/src/main.o
-POSSIBLE_DEPFILES=${OBJECTDIR}/src/driver/AS5055.o.d ${OBJECTDIR}/src/driver/ForgeHeater.o.d ${OBJECTDIR}/src/driver/SPIClient.o.d ${OBJECTDIR}/src/driver/FlashStorageLocal.o.d ${OBJECTDIR}/src/driver/Servo.o.d ${OBJECTDIR}/src/kinematics/KosselKinematics.o.d ${OBJECTDIR}/src/kinematics/delta.o.d ${OBJECTDIR}/src/kinematics/FrogLegs.o.d ${OBJECTDIR}/src/kinematics/CartesianController.o.d ${OBJECTDIR}/src/motion/Calibration.o.d ${OBJECTDIR}/src/motion/StepperSim.o.d ${OBJECTDIR}/src/motion/PID.o.d ${OBJECTDIR}/src/namespace/Callbacks.o.d ${OBJECTDIR}/src/namespace/Namespace_bcs_cartesian.o.d ${OBJECTDIR}/src/namespace/UpstreamPackets.o.d ${OBJECTDIR}/src/main.o.d
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/src/driver/AS5055.o ${OBJECTDIR}/src/driver/ForgeHeater.o ${OBJECTDIR}/src/driver/SPIClient.o ${OBJECTDIR}/src/driver/FlashStorageLocal.o ${OBJECTDIR}/src/driver/Servo.o ${OBJECTDIR}/src/kinematics/KosselKinematics.o ${OBJECTDIR}/src/kinematics/delta.o ${OBJECTDIR}/src/kinematics/FrogLegs.o ${OBJECTDIR}/src/kinematics/CartesianController.o ${OBJECTDIR}/src/motion/Calibration.o ${OBJECTDIR}/src/motion/StepperSim.o ${OBJECTDIR}/src/motion/PID.o ${OBJECTDIR}/src/namespace/Namespace_bcs_cartesian.o ${OBJECTDIR}/src/namespace/UpstreamPackets.o ${OBJECTDIR}/src/main.o
+POSSIBLE_DEPFILES=${OBJECTDIR}/src/driver/AS5055.o.d ${OBJECTDIR}/src/driver/ForgeHeater.o.d ${OBJECTDIR}/src/driver/SPIClient.o.d ${OBJECTDIR}/src/driver/FlashStorageLocal.o.d ${OBJECTDIR}/src/driver/Servo.o.d ${OBJECTDIR}/src/kinematics/KosselKinematics.o.d ${OBJECTDIR}/src/kinematics/delta.o.d ${OBJECTDIR}/src/kinematics/FrogLegs.o.d ${OBJECTDIR}/src/kinematics/CartesianController.o.d ${OBJECTDIR}/src/motion/Calibration.o.d ${OBJECTDIR}/src/motion/StepperSim.o.d ${OBJECTDIR}/src/motion/PID.o.d ${OBJECTDIR}/src/namespace/Namespace_bcs_cartesian.o.d ${OBJECTDIR}/src/namespace/UpstreamPackets.o.d ${OBJECTDIR}/src/main.o.d
 
 # Object Files
-OBJECTFILES=${OBJECTDIR}/src/driver/AS5055.o ${OBJECTDIR}/src/driver/ForgeHeater.o ${OBJECTDIR}/src/driver/SPIClient.o ${OBJECTDIR}/src/driver/FlashStorageLocal.o ${OBJECTDIR}/src/driver/Servo.o ${OBJECTDIR}/src/kinematics/KosselKinematics.o ${OBJECTDIR}/src/kinematics/delta.o ${OBJECTDIR}/src/kinematics/FrogLegs.o ${OBJECTDIR}/src/kinematics/CartesianController.o ${OBJECTDIR}/src/motion/Calibration.o ${OBJECTDIR}/src/motion/StepperSim.o ${OBJECTDIR}/src/motion/PID.o ${OBJECTDIR}/src/namespace/Callbacks.o ${OBJECTDIR}/src/namespace/Namespace_bcs_cartesian.o ${OBJECTDIR}/src/namespace/UpstreamPackets.o ${OBJECTDIR}/src/main.o
+OBJECTFILES=${OBJECTDIR}/src/driver/AS5055.o ${OBJECTDIR}/src/driver/ForgeHeater.o ${OBJECTDIR}/src/driver/SPIClient.o ${OBJECTDIR}/src/driver/FlashStorageLocal.o ${OBJECTDIR}/src/driver/Servo.o ${OBJECTDIR}/src/kinematics/KosselKinematics.o ${OBJECTDIR}/src/kinematics/delta.o ${OBJECTDIR}/src/kinematics/FrogLegs.o ${OBJECTDIR}/src/kinematics/CartesianController.o ${OBJECTDIR}/src/motion/Calibration.o ${OBJECTDIR}/src/motion/StepperSim.o ${OBJECTDIR}/src/motion/PID.o ${OBJECTDIR}/src/namespace/Namespace_bcs_cartesian.o ${OBJECTDIR}/src/namespace/UpstreamPackets.o ${OBJECTDIR}/src/main.o
 
 # Source Files
-SOURCEFILES=src/driver/AS5055.c src/driver/ForgeHeater.c src/driver/SPIClient.c src/driver/FlashStorageLocal.c src/driver/Servo.c src/kinematics/KosselKinematics.c src/kinematics/delta.c src/kinematics/FrogLegs.c src/kinematics/CartesianController.c src/motion/Calibration.c src/motion/StepperSim.c src/motion/PID.c src/namespace/Callbacks.c src/namespace/Namespace_bcs_cartesian.c src/namespace/UpstreamPackets.c src/main.c
+SOURCEFILES=src/driver/AS5055.c src/driver/ForgeHeater.c src/driver/SPIClient.c src/driver/FlashStorageLocal.c src/driver/Servo.c src/kinematics/KosselKinematics.c src/kinematics/delta.c src/kinematics/FrogLegs.c src/kinematics/CartesianController.c src/motion/Calibration.c src/motion/StepperSim.c src/motion/PID.c src/namespace/Namespace_bcs_cartesian.c src/namespace/UpstreamPackets.c src/main.c
 
 
 CFLAGS=
@@ -163,12 +163,6 @@ ${OBJECTDIR}/src/motion/PID.o: src/motion/PID.c  nbproject/Makefile-${CND_CONF}.
 	@${RM} ${OBJECTDIR}/src/motion/PID.o 
 	@${FIXDEPS} "${OBJECTDIR}/src/motion/PID.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1 -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION) -mips16 -O1 -I"include/replicator" -I"../../c-bowler/Platform/include" -I"../../c-bowler/BowlerStack/include" -MMD -MF "${OBJECTDIR}/src/motion/PID.o.d" -o ${OBJECTDIR}/src/motion/PID.o src/motion/PID.c   
 	
-${OBJECTDIR}/src/namespace/Callbacks.o: src/namespace/Callbacks.c  nbproject/Makefile-${CND_CONF}.mk
-	@${MKDIR} ${OBJECTDIR}/src/namespace 
-	@${RM} ${OBJECTDIR}/src/namespace/Callbacks.o.d 
-	@${RM} ${OBJECTDIR}/src/namespace/Callbacks.o 
-	@${FIXDEPS} "${OBJECTDIR}/src/namespace/Callbacks.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1 -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION) -mips16 -O1 -I"include/replicator" -I"../../c-bowler/Platform/include" -I"../../c-bowler/BowlerStack/include" -MMD -MF "${OBJECTDIR}/src/namespace/Callbacks.o.d" -o ${OBJECTDIR}/src/namespace/Callbacks.o src/namespace/Callbacks.c   
-	
 ${OBJECTDIR}/src/namespace/Namespace_bcs_cartesian.o: src/namespace/Namespace_bcs_cartesian.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} ${OBJECTDIR}/src/namespace 
 	@${RM} ${OBJECTDIR}/src/namespace/Namespace_bcs_cartesian.o.d 
@@ -259,12 +253,6 @@ ${OBJECTDIR}/src/motion/PID.o: src/motion/PID.c  nbproject/Makefile-${CND_CONF}.
 	@${RM} ${OBJECTDIR}/src/motion/PID.o.d 
 	@${RM} ${OBJECTDIR}/src/motion/PID.o 
 	@${FIXDEPS} "${OBJECTDIR}/src/motion/PID.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION) -mips16 -O1 -I"include/replicator" -I"../../c-bowler/Platform/include" -I"../../c-bowler/BowlerStack/include" -MMD -MF "${OBJECTDIR}/src/motion/PID.o.d" -o ${OBJECTDIR}/src/motion/PID.o src/motion/PID.c   
-	
-${OBJECTDIR}/src/namespace/Callbacks.o: src/namespace/Callbacks.c  nbproject/Makefile-${CND_CONF}.mk
-	@${MKDIR} ${OBJECTDIR}/src/namespace 
-	@${RM} ${OBJECTDIR}/src/namespace/Callbacks.o.d 
-	@${RM} ${OBJECTDIR}/src/namespace/Callbacks.o 
-	@${FIXDEPS} "${OBJECTDIR}/src/namespace/Callbacks.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION) -mips16 -O1 -I"include/replicator" -I"../../c-bowler/Platform/include" -I"../../c-bowler/BowlerStack/include" -MMD -MF "${OBJECTDIR}/src/namespace/Callbacks.o.d" -o ${OBJECTDIR}/src/namespace/Callbacks.o src/namespace/Callbacks.c   
 	
 ${OBJECTDIR}/src/namespace/Namespace_bcs_cartesian.o: src/namespace/Namespace_bcs_cartesian.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} ${OBJECTDIR}/src/namespace 
